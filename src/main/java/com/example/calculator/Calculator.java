252376f29837c5a56890a9ee9fbfcf500efed58e
unknown <khandelwal12nidhi@gmail.com>
2018-03-18 19:02:38 -0400

    Add Spring Boot skeleton

---
+++ src/main/java/com/example/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.calculator;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+     int sum(int a, int b) {
+          return a + b;
+     }
+}
